1d9c59dfe94d92f163a04d08491947ece67dfddb
rking <ryan.king@nrel.gov>
2019-03-20 16:46:22 -0600

    Gaussian hill troubleshooting

--- demos/Imported/ImportedMesh.py
+++ demos/Imported/ImportedMesh.py
@@ -11,8 +11,9 @@ farm = windse.GridWindFarm(dom)
 farm.Plot()
 
 ### Refine the Domain ###
-region = [[farm.ex_x[0],dom.x_range[1]],farm.ex_y,farm.ex_z]
-dom.Refine(1,region=region)
+# region = [[farm.ex_x[0],dom.x_range[1]],farm.ex_y,farm.ex_z]
+# region = [[-1000,2500],[-1000,1000],[0,216]]
+# dom.Refine(1,region=region)
 dom.Save()
 
 ### Function Space ###
@@ -20,13 +21,13 @@ fs = windse.LinearFunctionSpace(dom)
 print(fs.W.dim())
 
 ### Setup Boundary Conditions ###
-bc = windse.LinearInflow(dom,fs)
+bc = windse.PowerInflow(dom,fs)
 
 ### Generate the problem ###
 problem = windse.StabilizedProblem(dom,farm,fs,bc)
 
 ### Solve ###
-solver = windse.DefaultSolver(problem)
+solver = windse.SteadySolver(problem)
 solver.Solve()
 
 ### Output Results ###
